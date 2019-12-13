96e6c0102c9e77a6136c4732f1e6bec010898956
shiva.hanumandla <shiva.hanumandla@team.telstra.com>
2019-12-14 00:15:24 +0530

    Initial Commit

---
+++ bcms-resource-server/src/main/java/com/infy/controller/Controller.java
@@ -0,0 +1,69 @@
+package com.infy.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.infy.dao.EmployeeRepository;
+import com.infy.entity.Employee;
+
+@CrossOrigin(origins = "http://example.com")
+@RestController
+
+public class Controller {
+	@RequestMapping("/")
+	public String home() {
+		return "Spring boot is working!";
+	}
+
+	@RequestMapping("/try")
+	public String try_() {
+		return "I'm trying";
+	}
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@GetMapping("/getEmployeebyName")
+	public List<Employee> getProduct(@RequestParam(value = "id", defaultValue = "") String id) {
+		try {
+			List<Employee> l = employeeRepository.findByid(id);
+			return l;
+		} catch (Exception e) {
+			return Collections.emptyList();
+		}
+	}
+
+	@PostMapping("/addEmployee")
+	public ResponseEntity<Employee> addProduct(@RequestBody Employee employee) {
+		try {
+			employeeRepository.save(employee);
+			return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Employee>(employee, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/deleteEmployee")
+	public ResponseEntity<Employee> deleteProduct(@RequestParam(value = "dao") Employee employee) {
+		try {
+			employeeRepository.delete(employee);
+			return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Employee>(employee, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+}
