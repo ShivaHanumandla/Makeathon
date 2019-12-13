56c9978d309fcac0be4e3388b7de8d1163d5a8c4
shiva.hanumandla <shiva.hanumandla@team.telstra.com>
2019-12-14 00:26:26 +0530

    Done

---
+++ src/main/java/com/infy/dao/EmployeeRepository.java
@@ -0,0 +1,23 @@
+package com.infy.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.infy.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee ,Long>{
+    List<Employee> findByid(String searchKey);
+
+
+//    @Autowired
+//    JdbcTemplate jdbcTemplate;
+//
+//    public List<ProductEntity> searchProduct(String  productName) {
+//        return jdbcTemplate.query("select * from productEntity where productName=?", new Object[] { productName },
+//                new BeanPropertyRowMapper<ProductEntity>(ProductEntity.class));
+//    }
+
+}
